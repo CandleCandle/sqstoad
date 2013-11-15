f24817fe440e3195be44aad15425f1a7fe421f3c
Candle <candle@candle.me.uk>
2013-11-15 12:55:49 +0000

    better http(s) proxy handling, better help.

--- src/main/java/uk/me/candle/sqstoad/App.java
+++ src/main/java/uk/me/candle/sqstoad/App.java
@@ -2,7 +2,9 @@ package uk.me.candle.sqstoad;
 
 import com.amazonaws.ClientConfiguration;
 import com.google.common.collect.Lists;
+import java.net.URI;
 import java.util.Arrays;
+import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,16 +20,16 @@ public class App {
 
 		String command = args.length > 0 ? args[0] : "help";
 
+        boolean found = false;
         for (CliAction.Actions action : CliAction.Actions.values()) {
             if (action.getCommand().equals(command)) {
-                CliAction cmd = action.newInstance(conf);
-                CmdLineParser parser = new CmdLineParser(cmd);
-                LOG.debug("action: {}", cmd.getClass().getName());
-                LOG.debug("args: {}", Lists.newArrayList(Arrays.copyOfRange(args, 1, args.length)));
-                parser.parseArgument(Arrays.copyOfRange(args, 1, args.length));
 
-                cmd.call();
+                runCommand(action, conf, args);
+                found = true;
             }
+        }
+        if (!found) {
+            runCommand(CliAction.Actions.HELP, conf, args);
         }
 	}
 
@@ -36,11 +38,36 @@ public class App {
 		String proxyProp = System.getenv("https_proxy");
 		if (proxyProp != null) {
 			LOG.debug("Found an HTTPS proxy {}", proxyProp);
-			String[] parts = proxyProp.split(":");
-			cc.setProxyHost(parts[0]);
-			cc.setProxyPort(Integer.parseInt(parts[1]));
+            if (proxyProp.startsWith("http")) {
+                setProxyFromUri(proxyProp, cc);
+            } else {
+                setProxyFromBasic(proxyProp, cc);
+            }
+
 		}
 		return cc;
 	}
+
+    private static void setProxyFromBasic(String proxyProp, ClientConfiguration cc) {
+        String[] parts = proxyProp.split(":");
+        cc.setProxyHost(parts[0]);
+        cc.setProxyPort(Integer.parseInt(parts[1]));
+    }
+
+    private static void setProxyFromUri(String proxyProp, ClientConfiguration cc) {
+        URI uri = URI.create(proxyProp);
+        cc.setProxyHost(uri.getHost());
+        cc.setProxyPort(uri.getPort());
+    }
+
+    private static void runCommand(CliAction.Actions action, ClientConfiguration conf, String[] args) throws Exception, CmdLineException {
+        CliAction cmd = action.newInstance(conf);
+        CmdLineParser parser = new CmdLineParser(cmd);
+        LOG.debug("action: {}", cmd.getClass().getName());
+        LOG.debug("args: {}", Lists.newArrayList(Arrays.copyOfRange(args, 1, args.length)));
+        parser.parseArgument(Arrays.copyOfRange(args, 1, args.length));
+
+        cmd.call();
+    }
 
 }
